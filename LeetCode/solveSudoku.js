2c1dc42e6c0eca7aaef368b0ed30b09b33680bf0
Andrew <gandrejko2002@gmail.com>
2022-06-22 23:29:54 +0300

    add sudoku to LeetCode folder

--- LeetCode/solveSudoku.js
+++ LeetCode/solveSudoku.js
@@ -1,58 +1,98 @@
-var solveSudoku = function(board) {
-    const rowMain = [];
-    const colMain = [];
-    const areaMain = [];
-    
-    for (let i = 0; i < board.length; i++) {  
-        const row = [];
-        const col = [];
-        for (let j = 0; j < board[i].length; j++) {
-            row.push(board[i][j]);
-            col.push(board[j][i]);
-            if((i - 1) % 3 === 0 && (j - 1) % 3 === 0) {
-                const area = [];
-                for (let k = 0; k < 3; k++) {
-                    for (let l = 0; l < 3; l++) {
-                        area.push(board[i-1+k][j-1+l]);                       
-                    }                
-                }
-                areaMain.push(area);
-            }
-        }
-        rowMain.push(row);
-        colMain.push(col);
-    }
-    
-    
-    // console.log(areaMain);
-    // console.log(rowMain);
-    // console.log(colMain);
+function solveSudoku(board){
+	const rows = getRows(board);
+	const cols = getCols(board);
+	const areas = getAreas(board);
+
+	// console.log(areas);
+
+	for (let i = 0; i < 9; i++) {
+		for (let j = 0; j < 9; j++) {
+			if(getValue(i, j) != undefined && board[i][j] === "."){
+				board[i][j] = getValue(i, j);
+			}
+		}	
+	}
+	
+	
+	function getValue(posRow, posCol) {
+		const num = new Set(["1", "2", "3", "4", "5", "6", "7", "8", "9"]);
+		const row = rows[posRow];
+		const col = cols[posCol];
+		const area = areas[Math.floor(posRow/3)*3+Math.floor(posCol/3)];
+		const arr = [];
+		row.forEach(e => {
+			if(num.has(e)){
+				num.delete(e);
+			}
+		});
+		col.forEach(e => {
+			if(num.has(e)){
+				num.delete(e);
+			}
+		});	
+		area.forEach(e => {
+			e.forEach(elem => {
+				if(num.has(elem)){
+					num.delete(elem);
+				}
+			})			
+		});
 
 
-    let k = 0;
+		num.forEach(e => {
+			arr.push(e);
+		});
+		if(num.size === 1)  return arr[0];		
+	}
+	
+	function getRows(x){
+		const rowMain = [];
+		for (let i = 0; i < x.length; i++) {  
+			const row = [];
+			for (let j = 0; j < x[i].length; j++) {
+				row.push(x[i][j]);
+			}
+			rowMain.push(row);
+		}
+		return rowMain;
+	}
+	
+	function getCols(x){
+		const colMain = [];
+		for (let i = 0; i < x.length; i++) {  
+			const col = [];
+			for (let j = 0; j < x[i].length; j++) {
+				col.push(x[j][i]);
+			}
+			colMain.push(col);
+		}
+		return colMain;
+	}
+	function getAreas(x){
+		const areaMain = [];
+		for (let i = 0; i < 9; i += 3) {
+			for (let j = 0; j < 9; j += 3){
+				const area = [];
+				for (let k = 0; k < 3; k++) {
+					const areaRow = [];
+					for (let l = 0; l < 3; l++) {
+						areaRow.push(x[i+k][j+l]);
+					}	
+					area.push(areaRow);
+				}
+				areaMain.push(area);
+			}
+		}
+		return areaMain;
+	}
     for (let i = 0; i < 9; i++) {
-        for (let j = 0; j < 9; j++) {
-            
-            // console.log(rowMain[i][j], colMain[j][i]);
-            // console.log('=============');
-
-            if(rowMain[i][j] === '.'){
-                console.log(rowMain[i].includes('5'));
-                ++k;
-            }
-        }        
-    }
-    console.log("k", k);
-
-    function isInclude(index, arr) {
-        for (index = 0; index < arr.length; index++) {
-            
+        if(board[i].includes('.')){
+            solveSudoku(board);
         }
     }
-
-
-
-};
+    console.log(board);
+	return board;
+}
 
 
 
