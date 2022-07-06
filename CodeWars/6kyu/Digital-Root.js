879cfb3cb9225c480fb3daf1876d77579bb57eef
Andrew <gandrejko2002@gmail.com>
2022-07-06 11:35:20 +0300

    add digital Root solution

--- CodeWars/6kyu/Digital-Root.js
+++ CodeWars/6kyu/Digital-Root.js
@@ -1,3 +1,11 @@
 function digital_root(n) {
-    
+    const num = n + '';
+    let res = 0;
+    for (let i = 0; i < num.length; i++) {
+        res += +num[i];
+    }
+    if(n > 10) {
+        res = digital_root(res);
+    }
+    return res;
 }
