a1d5cc42a07f22821bdcac29a0be0505c14f636e
Andrew <gandrejko2002@gmail.com>
2022-07-17 10:29:31 +0300

    add method for columns

--- inbox/sudoku/script.js
+++ inbox/sudoku/script.js
@@ -8,6 +8,18 @@
 // 81 * 81 * 27
 let DEBUG = false;
 
+const defaultTask = [
+	["", "", "", "", "", "", "", "", ""],
+	["", "", "", "", "", "", "", "", ""],
+	["", "", "", "", "", "", "", "", ""],
+	["", "", "", "", "", "", "", "", ""],
+	["", "", "", "", "", "", "", "", ""],
+	["", "", "", "", "", "", "", "", ""],
+	["", "", "", "", "", "", "", "", ""],
+	["", "", "", "", "", "", "", "", ""],
+	["", "", "", "", "", "", "", "", ""]
+];
+
 const task1 = [
 	["", "4", "", "1", "3", "8", "9", "5", "7"],
 	["7", "3", "1", "", "9", "5", "6", "", "8"],
@@ -54,6 +66,66 @@ const task4 = [
 	["", "", "", "8", "", "3", "", "2", ""],
 	["", "", "", "", "", "", "", "", "6"],
 	["", "", "", "2", "7", "5", "9", "", ""]
+];
+
+const task5 = [
+	["5", "", "", "", "3", "", "", "", ""],
+	["", "", "", "", "", "", "", "4", "1"],
+	["", "", "", "1", "", "4", "7", "", ""],
+	["", "", "", "", "", "", "", "", ""],
+	["6", "", "", "", "", "", "5", "", ""],
+	["", "", "", "8", "", "", "", "", ""],
+	["", "8", "", "2", "", "7", "", "", ""],
+	["", "", "", "", "6", "", "3", "9", ""],
+	["", "1", "", "", "", "", "", "", ""]
+];
+
+const task6 = [
+	["9", "", "", "5", "", "8", "", "4", ""],
+	["", "", "", "", "", "", "", "", ""],
+	["6", "4", "", "", "9", "7", "", "", "5"],
+	["8", "", "6", "4", "", "", "5", "9", ""],
+	["", "", "", "", "", "", "", "", ""],
+	["7", "", "", "", "", "", "", "6", "4"],
+	["", "6", "5", "", "4", "2", "7", "3", ""],
+	["", "", "9", "", "7", "", "", "5", "6"],
+	["", "8", "", "6", "", "5", "4", "", ""]
+];
+
+const task7 = [
+	["", "", "5", "", "", "", "", "", ""],
+	["", "7", "", "9", "", "", "", "6", ""],
+	["9", "", "2", "", "1", "", "", "7", ""],
+	["5", "", "", "6", "", "2", "4", "", ""],
+	["", "4", "", "", "3", "", "", "", "2"],
+	["", "2", "", "", "", "", "3", "1", ""],
+	["", "9", "6", "1", "", "", "", "", ""],
+	["2", "", "", "", "9", "", "", "", ""],
+	["", "", "3", "2", "", "4", "6", "", "1"]
+];
+
+const task8 = [
+	["", "9", "4", "", "", "", "5", "", "7"],
+	["", "7", "", "", "8", "", "", "", ""],
+	["", "", "6", "", "", "4", "1", "2", ""],
+	["", "", "1", "", "", "", "", "", "2"],
+	["", "", "", "8", "6", "", "9", "7", ""],
+	["", "6", "", "2", "", "7", "", "", ""],
+	["2", "3", "", "4", "1", "8", "7", "", "6"],
+	["6", "4", "", "7", "", "5", "3", "1", "9"],
+	["5", "1", "", "", "9", "", "", "", ""]
+];
+
+const task9 = [
+	["", "7", "", "9", "", "1", "", "", ""],
+	["", "", "9", "", "", "", "", "", ""],
+	["", "", "1", "", "", "4", "", "", "2"],
+	["5", "", "", "", "", "", "7", "", ""],
+	["7", "", "8", "", "", "", "2", "", ""],
+	["", "", "", "", "", "", "1", "9", "4"],
+	["", "", "", "7", "", "", "", "", "6"],
+	["", "", "", "", "2", "", "", "4", ""],
+	["3", "6", "", "", "8", "", "", "", ""]
 ];
 
 function getCol(posCol, board) {
@@ -113,19 +185,50 @@ function findDiffValueRow(posRow, board) {
 	}
 
 	for (let i = 1; i < resRow.length + 1; i++) {
-		let element = 9;
 		const arr = [];
 		for (let j = 0; j < 9; j++) {
 			if(!resRow[j][1].includes((i).toString())) arr.push(j);		
 		}
 		if(arr.length === 1) {
-			result = i;	
+			result = i.toString();	
 			index = arr[0];		
 		}
 	}
 
 	return [result, index];
 }
+
+function findDiffValueCol(posCol, board) {
+	const col = getCol(posCol, board);
+	const resCol = [];
+	let result = 0;
+	let index = 0;
+
+	for (let i = 0; i < col.length; i++) {
+		const element = [i];
+		if(col[i] === '') {
+			element[1] = getValue(i, posCol, board)[1].filter(Boolean);
+		} else{
+			element[1] = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+		}
+		resCol.push(element);
+	}
+
+	for (let i = 1; i < resCol.length + 1; i++) {
+		const arr = [];
+		for (let j = 0; j < 9; j++) {
+			if(!resCol[j][1].includes((i).toString())) arr.push(j);		
+		}
+		if(arr.length === 1) {
+			result = i.toString();	
+			index = arr[0];		
+		}
+	}
+
+	return [result, index];
+}
+
+
 
 
 
@@ -138,11 +241,22 @@ function sudokuSolver(board) {
 			if (board[i][j] === "" && res[0].length === 1) {
 				board[i][j] = res[0][0];
 			}
+			if(i === 8) {
+				const resDiffCol = findDiffValueCol(j, board);
+				if(resDiffCol[1] !== 0) {
+					board[resDiffCol[1]][j] = resDiffCol[0];
+				}
+			}			
 		}
 		const resDiffRow = findDiffValueRow(i, board);
-		if(resDiffRow[0] !== 0) {
+		
+		if(resDiffRow[1] !== 0) {
 			board[i][resDiffRow[1]] = resDiffRow[0];
-		}
+		}		
+	}
+
+	if(board.flat().includes('')) {
+		sudokuSolver(board);
 	}
 }
 
@@ -160,8 +274,49 @@ function getTask(task) {
 
 	for (let i = 0; i < items.length; i++) {
 		items[i].innerHTML = arr[i];
+		if(arr[i] === '') items[i].classList.add('empty');
 	}
 }
+
+function getUselessNumbers(task) {
+	const result = [];
+	for (let i = 0; i < task.length; i++) {
+		for (let j = 0; j < task[i].length; j++) {
+			if(task[i][j] === '') {
+				result.push(getValue(i, j, task)[0].filter(x => x != ''));
+			} else {
+				result.push('');
+			}
+		}
+	}
+	return result;
+}
+
+
+function deleteUselessNumbers(task) {
+	const items = document.querySelectorAll(".item");
+	const result = getUselessNumbers(task);
+	
+
+	for (let i = 0; i < result.length; i++) {
+		for (let j = 1; j <= 9; j++) {
+			if(result[i].includes(j.toString())) items[i].innerHTML += `<div class="value">${j}</div>`;
+			else items[i].innerHTML += `<div class="value"></div>`;
+		}
+	}
+}
+
+function colorizeValue(number, color) {
+	const values = document.querySelectorAll('.value');
+
+	values.forEach(e => {
+		if(e. innerHTML == number) {
+			e.classList.add('color');
+			e.style.background = color;
+		}
+	});
+}
+
 
 function addItems() {
 	const sudoku = document.querySelector(".sudoku");
@@ -171,30 +326,22 @@ function addItems() {
 
 
 window.addEventListener("DOMContentLoaded", () => {
-	const task = task4;
+	const task = task8;
 	const btn = document.querySelector(".solver");
 
 	addItems();
 	getTask(task);
+	deleteUselessNumbers(task);
 
 	btn.addEventListener("click", () => {
+		getValue(2, 8, task4)[0];
 		sudokuSolver(task);
 		getTask(task);
+		deleteUselessNumbers(task);
+
+		// colorizeValue(3, 'green');
+		// colorizeValue(8, 'red');
+		// colorizeValue(7, 'yellow');
+		// colorizeValue(5, 'aqua');
 	});
 });
-
-
-// function getDiffValue(posRow, posCol, board) {
-// 	const numDiff = new Set();
-// 	const row = board[posRow]; //Get row from board
-// 	const col = getCol(posCol, board); //Get column from board
-// 	const area = fillSquare(board, Math.floor(posRow / 3) * 3, Math.floor(posCol / 3) * 3); //Get area from board
-
-// 	row
-// 	.concat(col)
-// 	.concat(area)
-// 	.forEach((e) => {
-// 		numDiff.add(e);
-// 	});	
-// 	return [...numDiff];
-// }
