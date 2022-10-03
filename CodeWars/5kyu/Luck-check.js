a3883ece3e3531fc04ae10f4dfb1216bc1ef8e11
Andrew <gandrejko2002@gmail.com>
2022-10-03 14:24:08 +0300

    add likes solution

--- CodeWars/5kyu/Luck-check.js
+++ CodeWars/5kyu/Luck-check.js
@@ -1,13 +1,16 @@
 function luckCheck(ticket){
+    if(!+ticket){
+        throw new Error();
+    }
     let left = 0;
     let right = 0;
     for (let i = 0; i < ticket.length/2; i++) {
         left += +ticket[i];
         right += +ticket[ticket.length/2 + i];
     }
-    if(right === left) {
+    if(right === left && ticket.length % 2 == 0) {
         return true;
     }
     return false;
 }
-console.log(luckCheck('683179'));
+console.log(luckCheck('62428'));
