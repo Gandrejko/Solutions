7a19f5bc7927aa904b41001fea068a57978b93d5
Andrew <gandrejko2002@gmail.com>
2022-10-11 11:21:50 +0300

    make factorial with recursion

--- CodeWars/7kyu/Factorial.js
+++ CodeWars/7kyu/Factorial.js
@@ -1,5 +1,10 @@
-function factorial(n){
-    let res = 1;
-    for (let i = 1; i <= n; i++) res *= i;
-    return res;
-}
+function factorial (n) {
+    if(n < 0) {
+      return null;
+    } else if(n == 0) {
+      return 1; 
+    } else {
+      return n*factorial(n-1);
+    }
+  }
+console.log(factorial(5))
