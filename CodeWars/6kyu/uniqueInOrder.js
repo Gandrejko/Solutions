8dcbb188295d71174b63b108bc13261ac6679488
Andrew <gandrejko2002@gmail.com>
2022-10-13 11:13:34 +0300

    add uniqueInOrder solution

---
+++ CodeWars/6kyu/uniqueInOrder.js
@@ -0,0 +1,9 @@
+let uniqueInOrder = function(iterable){
+    const result = [];
+    for (let i = 0; i < iterable.length; i++) {
+        if(iterable[i] !== iterable[i - 1]) result.push(iterable[i])
+    }
+    return result
+}
+
+console.log(uniqueInOrder('AAAABBBCCDAABBB'))
